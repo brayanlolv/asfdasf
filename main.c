528ebc9ae5f925c1cc9a0ec5d09d3b7f2da25ab8
brayanlolv <122327255+brayanlolv@users.noreply.github.com>
2023-10-25 23:03:40 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,133 +1,128 @@
+#include "func.h"
+#include "stack.h"   //arquivo q tera as funcoes relacionadas a pilha
+#include "structs.h" //arquivo com as structs grafo, vertice
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <strings.h>
-#include "func.h"
-#include <ctype.h>
-#include "structs.h" //arquivo com as structs grafo, vertice
-#include "stack.h"   //arquivo q tera as funcoes relacionadas a pilha
 
 char comandolimpar[6] = "cls"; // para windows, se for linux mudar isso
 
 // se mudar o escolher processo de document apaga
-void modificarMatriz(int *matriz, int V0, int V1, int valor, struct Grafo *grafo); // se mudar o escolher processo de document apaga
-void dfsPilha(int verticeinicial, struct Grafo *grafo, int *matrizAdj);
+void modificarMatriz(
+    int *matriz, int V0, int V1, int valor,
+    struct Grafo *grafo); // se mudar o escolher processo de document apaga
+void dfsPilha(int verticeinicial, int vertices, int *matrizAdj,
+              struct Grafo grafo);
 void escolherProcesso(int *matriz, int length, struct Grafo grafo);
 void printMatriz(int *matriz, int length, struct Grafo grafo);
 void initMatriz(int *matriz, int length);
-int main()
-{
-    int digrafo;
-    struct Grafo grafo = {0, 0, 0};
-    printf("se o grafo for um digrafo (direcionado) digite 1 ?\n");
-    scanf("%d", &digrafo);
-    if (digrafo == 1)
-    {
-        grafo.digrafo = 1;
-    }
-    printf("quantos vertices tera grafo ?\n");
-    scanf("%d", &grafo.vertice);
-    printf("seu grafo tem %d vertices e %d arestas", grafo.vertice, grafo.arestasGrafo);
+int main() {
+  int digrafo;
+  struct Grafo grafo = {0, 0, 0};
+  printf("se o grafo for um digrafo (direcionado) digite 1 ?\n");
+  scanf("%d", &digrafo);
+  if (digrafo == 1) {
+    grafo.digrafo = 1;
+  }
+  printf("quantos vertices tera grafo ?\n");
+  scanf("%d", &grafo.vertice);
+  printf("seu grafo tem %d vertices e %d arestas", grafo.vertice,
+         grafo.arestasGrafo);
 
-    // iniciar matriz, no caso atribuir 0 e printar ela
-    int matrizAdjacencia[grafo.vertice][grafo.vertice];
-    initMatriz(&matrizAdjacencia[0][0], grafo.vertice);
-    printMatriz(&matrizAdjacencia[0][0], grafo.vertice, grafo);
-    ////
-    escolherProcesso(&matrizAdjacencia[0][0], grafo.vertice, grafo);
+  // iniciar matriz, no caso atribuir 0 e printar ela
+  int matrizAdjacencia[grafo.vertice][grafo.vertice];
+  initMatriz(&matrizAdjacencia[0][0], grafo.vertice);
+  printMatriz(&matrizAdjacencia[0][0], grafo.vertice, grafo);
+  ////
+  escolherProcesso(&matrizAdjacencia[0][0], grafo.vertice, grafo);
 
-    return 7;
+  return 7;
 };
 
-// printf("1.adicionar vertice.\n2.remover vertice.\n qualquer coisa para voltar");
-void modificarMatriz(int *matriz, int V0, int V1, int valor, struct Grafo *grafo)
-{
+// printf("1.adicionar vertice.\n2.remover vertice.\n qualquer coisa para
+// voltar");
+void modificarMatriz(int *matriz, int V0, int V1, int valor,
+                     struct Grafo *grafo) {
 
-    if (valor == 0) // zerar o valor da aresta é considerado removela
-    {
-        grafo->arestasGrafo = grafo->arestasGrafo - 1;
-    }
-    else
-    {
-        grafo->arestasGrafo = grafo->arestasGrafo + 1;
-    }
+  if (valor == 0) // zerar o valor da aresta é considerado removela
+  {
+    grafo->arestasGrafo = grafo->arestasGrafo - 1;
+  } else {
+    grafo->arestasGrafo = grafo->arestasGrafo + 1;
+  }
 
-    int i = V0 - 1;
-    int j = V1 - 1;
+  int i = V0 - 1;
+  int j = V1 - 1;
 
-    *(matriz + j + (i * grafo->vertice)) = valor;
+  *(matriz + j + (i * grafo->vertice)) = valor;
 
-    if (grafo->digrafo != 1) // se o grafo nao for um digrafo o mesmo valor sera associado tanto a i->j tanto j->i
-    {
-        *(matriz + i + (j * grafo->vertice)) = valor;
-    }
+  if (grafo->digrafo != 1) // se o grafo nao for um digrafo o mesmo valor sera
+                           // associado tanto a i->j tanto j->i
+  {
+    *(matriz + i + (j * grafo->vertice)) = valor;
+  }
 };
 
-void escolherProcesso(int *matriz, int length, struct Grafo grafo)
-{
-    printf("\ndigite:\n1.para modificar um vertice.\n2.para calcular");
+void escolherProcesso(int *matriz, int length, struct Grafo grafo) {
+  printf("\ndigite:\n1.para modificar um vertice.\n2.para calcular");
 
+  int escolha = 0;
+  int verticeTarefa;
+  scanf("%d", &escolha);
+  if (escolha == 1) { // qual vertice modificar?
+
+    system(comandolimpar);
+    printMatriz(matriz, length, grafo);
     int escolha = 0;
-    int verticeTarefa;
-    scanf("%d", &escolha);
-    if (escolha == 1)
-    { // qual vertice modificar?
+    int vertice0, vertice1, valor; // por um float para valor
+    printf(
+        "\n qual vertice desejas modificar ?\nselecione o vertice de  1 a %d\n",
+        length);
+    scanf("%d", &vertice0);
+    printf(" com qual outro vertice desejas mudar sua adjacencia(aresta)\n");
+    scanf("%d", &vertice1);
+    printf("qual valor desejas atribuir a esse vertice ?\n");
+    scanf("%d", &valor);
 
-        system(comandolimpar);
-        printMatriz(matriz, length, grafo);
-        int escolha = 0;
-        int vertice0, vertice1, valor; // por um float para valor
-        printf("\n qual vertice desejas modificar ?\nselecione o vertice de  1 a %d\n", length);
-        scanf("%d", &vertice0);
-        printf(" com qual outro vertice desejas mudar sua adjacencia(aresta)\n");
-        scanf("%d", &vertice1);
-        printf("qual valor desejas atribuir a esse vertice ?\n");
-        scanf("%d", &valor);
+    // verificar se o vertice escolhido existe
 
-        // verificar se o vertice escolhido existe
+    modificarMatriz(matriz, vertice0, vertice1, valor, &grafo);
+    system(comandolimpar);
+    printMatriz(matriz, length, grafo);
+    escolherProcesso(matriz, length, grafo);
+  } else if (escolha == 2) {
+    static char temp[256];
+    fgets(temp, sizeof(temp), stdin); // limpar o q ficou no buffer
 
-        modificarMatriz(matriz, vertice0, vertice1, valor, &grafo);
-        system(comandolimpar);
-        printMatriz(matriz, length, grafo);
-        escolherProcesso(matriz, length, grafo);
-    }
-    else if (escolha == 2)
-    {
-        static char temp[256];
-        fgets(temp, sizeof(temp), stdin); // limpar o q ficou no buffer
-
-        int verticeInicial;
-        printf("de qual verticevc quer comecaro dfs ? \n\n\n");
-        scanf("&d",&verticeInicial);
-        system(comandolimpar);
-        dfsPilha(verticeInicial,&grafo,matriz);
-    }
-    else
-    {
-        static char temp[256];
-        fgets(temp, sizeof(temp), stdin); // limpar o q ficou no buffer
-        escolherProcesso(matriz, length, grafo);
-    }
+    int verticeInicial;
+    printf("de qual verticevc quer comecaro dfs ? \n\n\n");
+    scanf("%d", &verticeInicial);
+    system(comandolimpar);
+    dfsPilha(verticeInicial, grafo.vertice, matriz, grafo);
+  } else {
+    static char temp[256];
+    fgets(temp, sizeof(temp), stdin); // limpar o q ficou no buffer
+    escolherProcesso(matriz, length, grafo);
+  }
 }
 
-void printMatriz(int *matriz, int length, struct Grafo grafo)
-{
+void printMatriz(int *matriz, int length, struct Grafo grafo) {
 
-    printf("\n matriz de adjacencia\n");
+  printf("\n matriz de adjacencia\n");
 
-    for (int i = 0; i < length; i++)
-    {
-        printf("\033[4m   %d\033[0m", i + 1);
+  for (int i = 0; i < length; i++) {
+    printf("\033[4m   %d\033[0m", i + 1);
+  }
+
+  for (int i = 0; i < length; i++) {
+    printf("\n%d|", i + 1);
+    for (int j = 0; j < length; j++) {
+      printf(" %d  ", *(matriz + j + (i * length)));
     }
-
-    for (int i = 0; i < length; i++)
-    {
-        printf("\n%d|", i + 1);
-        for (int j = 0; j < length; j++)
-        {
-            printf(" %d  ", *(matriz + j + (i * length)));
-        }
-    }
-    printf(" \no grafo tem %d vertices e %d arestas", grafo.vertice, grafo.arestasGrafo);
+  }
+  printf(" \no grafo tem %d vertices e %d arestas", grafo.vertice,
+         grafo.arestasGrafo);
 }
 
 // void execDfs(int verticeinicial, struct Grafo *grafo, int *matrizAdj)
@@ -142,34 +137,55 @@ void printMatriz(int *matriz, int length, struct Grafo grafo)
 //     dfsPilha(verticeInicial, &grafo, matrizAdj);
 // }
 
-void dfsPilha(int verticeinicial, struct Grafo *grafo, int *matrizAdj)
-{
-    verticeinicial -= 1; // se pah tapvez temja q tirar isso
+void dfsPilha(int verticeinicial, int vertices, int *matrizAdj,
+              struct Grafo grafo) {
+  verticeinicial -= 1; // se pah tapvez temja q tirar isso
+  // printf("%d",verticeinicial);
+  int verticesvisitados[vertices];
+  setarArray(&verticesvisitados[0], vertices, 0);
+  // verificar se o vertice ja foi visitado
 
-    int verticesvisitados[grafo->vertice];
-    setarArray(&verticesvisitados[0], grafo->vertice, 0);
-    // verificar se o vertice ja foi visitado
+  // criar pilha q vai guardar os vertices visitados
+  struct Pilha *pilha = criarPilha();
+  struct Pilha *pesos = criarPilha();
+  // empilhar o vertice inicial
+  empilhar(pilha,
+           verticeinicial); // talvez tenh q por um -1 no vertice inicial !!!!!!
 
-    // criar pilha q vai guardar os vertices visitados
-    struct Pilha *pilha = criarPilha();
-    // empilhar o vertice inicial
-    empilhar(pilha, verticeinicial); // talvez tenh q por um -1 no vertice inicial  !!!!!!
-
-    while (!estaVazia(pilha))
-    { // enquanto estiver vazia
-        int vertice = elementoTopo(pilha);
-        desempilhar(pilha);
-        verticesvisitados[vertice] = 1;
-        printf("%d  ", vertice + 1);
-
-        for (int i = 0; i < grafo->vertice; i++)
-        { // for (auto e : adj) isso é um um map, o e é o elemento, o adj é uum vetor;
-            int elemento = *(matrizAdj + (vertice * grafo->vertice) + i);
-            if (verticesvisitados[i] == 0 && elemento != 0)
-            {
-                empilhar(pilha, i);
-                i = grafo->vertice;
-            }
-        }
+  while (!estaVazia(pilha)) { // enquanto estiver vazia
+    int temVertice = 0;
+    int vertice = elementoTopo(pilha);
+    //desempilhar(pilha);//nao -e prara deempilhar todod hora
+    if(verticesvisitados[vertice] == 0){
+       printf("%d  ", vertice + 1);
     }
+   
+    verticesvisitados[vertice] = 1;
+
+    
+    for (int i = 0; i < vertices; i++) { // procurar alguma adjacencia no grafo
+                                         // o e é o elemento, o adj é uum vetor;
+      int elemento = *(matrizAdj + (vertice * vertices) + i);
+      if (verticesvisitados[i] == 0 && elemento != 0) {
+        empilhar(pilha, i);
+        temVertice =1;
+        //se o vertice a foi visitado nao é pra ser empilhado, mas, aoser desempilhado ele podeter suas vertices repesquisadas.
+        //empilhar(pesos, elemento); // jogar os pesos das arestas
+        i = vertices;
+      }
+    }
+
+      if(temVertice==0){
+        desempilhar(pilha);
+      }
+    
+  }
+  
+  //printf("top %d",pesos->topo);
+  //printf("\nmostrar os pesos dos vertices\n");
+  for(int i = 0 ;i < pesos->topo;i++){// resolvr esse bo
+    printf("e  ");
+    printf("%d  ",*(pesos->itens+i));
+  }
+  escolherProcesso(matrizAdj, vertices, grafo);
 }
--- stack.h
+++ stack.h
@@ -1,55 +1,59 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-
-
-struct Pilha{
-    int * itens;
-    int capacidade;
-    int topo;
-
+struct Pilha {
+  int *itens;
+  int capacidade;
+  int topo;
 };
 
-struct Pilha* criarPilha(){ // a funcao retorna um ponteiro
+struct Pilha *criarPilha() { // a funcao retorna um ponteiro
 
-struct Pilha * pilha = (struct Pilha*)malloc(sizeof(struct Pilha));//a pilha toda é um ponteiro,
-pilha->capacidade = 1;
-pilha->topo = -1; //setinha aponta para o ponteiro, difierentedo dot
-pilha->itens = (int *)malloc(pilha->capacidade * sizeof(int));
-return pilha;
+  struct Pilha *pilha = (struct Pilha *)malloc(
+      sizeof(struct Pilha)); // a pilha toda é um ponteiro,
+  pilha->capacidade = 1;
+  pilha->topo = -1; // setinha aponta para o ponteiro, difierentedo dot
+  pilha->itens =
+      (int *)malloc(pilha->capacidade * sizeof(int)); // esse 10 é gambiarra
+  return pilha;
 };
 
+int estaVazia(struct Pilha *pilha) { return pilha->topo == -1; };
 
-
-int estaVazia(struct Pilha *pilha){
-    return pilha->topo == -1; 
-};
-
-
-void empilhar(struct Pilha * pilha, int item){
-  pilha->itens[++pilha->topo]=item;
-   // printf("%d empilhado na pilha",item);
-   }
-
-    
-int desempilhar(struct Pilha * pilha){
-    if(estaVazia(pilha)){
-     // printf("pilha esta vazia, nada a desempilhar \n");
-      return -1;
-    }
-    return pilha->itens[pilha->topo--];}
-
-    
-int elementoTopo(struct Pilha *pilha){
-    if(!estaVazia(pilha)){
-        return pilha->itens[pilha->topo];
-    }
-    
+void redimencionarPilha(struct Pilha *pilha, int novaCapacidade) {
+  pilha->capacidade = novaCapacidade;
+  pilha->itens = (int *)realloc(pilha->itens, pilha->capacidade * sizeof(int));
+}
+//
+void empilhar(struct Pilha *pilha, int item) {
+  if (pilha->topo == pilha->capacidade - 2) { // teste
+    // se a pilha estiver cheia, redimensione para o dobro da capacidade
+    //printf("capacidade 1 = %d", pilha->capacidade);
+    pilha->capacidade *= 2;
+    //printf("capacidade 1 = %d", pilha->capacidade);
+    pilha->itens =(int *)realloc(pilha->itens, pilha->capacidade * sizeof(int));
+  }
+  pilha->itens[++pilha->topo] = item;
+  // printf("%d empilhado na pilha\n", item);
 }
 
-void setarArray(int * array,int length,int valor){
-    
-    for(int i = 0; i < length; i++){
-      *(array + i) = valor;
-    }
+int desempilhar(struct Pilha *pilha) {
+  if (estaVazia(pilha)) {
+    // printf("pilha esta vazia, nada a desempilhar \n");
+    return -1;
+  }
+  return pilha->itens[pilha->topo--];
+}
+
+int elementoTopo(struct Pilha *pilha) {
+  if (!estaVazia(pilha)) {
+    return pilha->itens[pilha->topo];
+  }
+}
+
+void setarArray(int *array, int length, int valor) {
+
+  for (int i = 0; i < length; i++) {
+    *(array + i) = valor;
+  }
 }
